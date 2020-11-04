bec3d6266cb897a84924b6447b249cbf59005707
jude taylor <512447@escg.ac.uk>
2020-11-04 15:30:04 +0000

    Tutorial part 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody theRB;
+    //public Rigidbody theRB;
     public float jumpForce;
+    public CharacterController controller;
+    public float gravityScale;
+
+    private Vector3 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        theRB = GetComponent<Rigidbody>();
+        //theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        //theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
     
-        if(Input.GetButtonDown("Jump"))
+        /*if(Input.GetButtonDown("Jump"))
         {
-            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);*/
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
         }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+    
     }
 }
