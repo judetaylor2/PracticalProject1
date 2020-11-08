385708e63412b51726c2d67783a02d76a94853d0
jude taylor <512447@escg.ac.uk>
2020-11-08 20:01:39 +0000

    Tutorial part 14

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -10,6 +10,13 @@ public class HealthManager : MonoBehaviour
 
     public PlayerController thePlayer;
 
+    public float invincibilityLength;
+    private float invincibilityCounter;
+
+    public Renderer playerRenderer;
+    private float flashCounter;
+    public float flashLength = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +28,39 @@ public class HealthManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(invincibilityCounter > 0)
+        {
+            invincibilityCounter -= Time.deltaTime;
+
+            flashCounter -= Time.deltaTime;
+            if (flashCounter <= 0)
+            {
+                playerRenderer.enabled = !playerRenderer.enabled;
+                flashCounter = flashLength;
+            }
+
+            if(invincibilityCounter <= 0)
+            {
+                playerRenderer.enabled = true;
+            }
+        }
     }
 
     public void HurtPlayer(int damage, Vector3 direction)
     {
-        currentHealth -= damage;
+        if(invincibilityCounter <= 0)
+        {
+            currentHealth -= damage;
 
-        thePlayer.KnockBack(direction);
+            thePlayer.KnockBack(direction);
+
+            invincibilityCounter = invincibilityLength;
+
+            playerRenderer.enabled = false;
+            flashCounter = flashLength;
+        }
+        
     }
 
     public void HealPlayer(int healAmount)
