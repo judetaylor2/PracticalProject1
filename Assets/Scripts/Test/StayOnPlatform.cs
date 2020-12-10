ab4426284de657950127718fc9464af5c0ab8ef0
512477 <512447@chrome.escg.ac.uk>
2020-12-10 15:45:18 +0000

    More experimentation with level design and textures

--- Assets/Scripts/Test/StayOnPlatform.cs
+++ Assets/Scripts/Test/StayOnPlatform.cs
@@ -7,6 +7,11 @@ public class StayOnPlatform : MonoBehaviour
     //variables
     public GameObject player;
 
+    void start()
+    {
+        
+    }
+
     //if the player is in the trigger, then they will become a child of the platform meaning that it will move with it
     private void OnTriggerStay(Collider other)
     {
---
+++ Assets/Scripts/Test/WaterMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMove : MonoBehaviour
+{
+    public float speed;
+    public Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //renderer = GetComponent<Renderer>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+
+        renderer.material.SetTextureOffset("_MainTex", new Vector2 (offset, 0));
+    }
+}
